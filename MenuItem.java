b7a81f0a964645f7cf45be9f405b9cdf3f385a00
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-03-25 18:13:56 -0400

    Create MenuItem.java

---
+++ MenuItem.java
@@ -0,0 +1,10 @@
+package Project4CS213;
+/**
+ * This is an abstract class to represent menu items
+ * Contains common operations for each menu item such as getting
+ * the price
+ * @author Christian Osma, Liam Smith
+ */
+public abstract class MenuItem { 
+    public abstract double getPrice();
+}
