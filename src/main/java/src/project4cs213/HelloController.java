de4961fcc33366e0c992f90ec2da8ed25fa8afc7
LTS2933 <116848955+LTS2933@users.noreply.github.com>
2023-03-31 14:18:11 -0400

    Add files via upload

---
+++ src/main/java/src/project4cs213/HelloController.java
@@ -0,0 +1,22 @@
+package src.project4cs213;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Button orderDonutsButton;
+    @FXML
+    private Button orderCoffeeButton;
+    @FXML
+    private Button basketButton;
+    @FXML
+    private Button placedOrdersButton;
+
+
+
+
+
+}
---
+++ src/main/java/src/project4cs213/SceneController.java
@@ -0,0 +1,35 @@
+package src.project4cs213;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    protected void showDonutOrderScreen(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("OrderingDonutsView.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void showCoffeeOrderScreen(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("OrderingCoffeeView.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
