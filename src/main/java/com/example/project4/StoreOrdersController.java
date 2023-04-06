9929e2915656d98ecda6ad6fc3202807422bdf07
LTS2933 <116848955+LTS2933@users.noreply.github.com>
2023-04-06 10:37:06 -0400

    Update StoreOrdersController.java

--- src/main/java/com/example/project4/StoreOrdersController.java
+++ src/main/java/com/example/project4/StoreOrdersController.java
@@ -116,7 +116,11 @@ public class StoreOrdersController implements Initializable {
             ordersError.setText(e.toString());
         }
     }
-
+    /**
+     * Event handler that changes the view to the MainView when the button to
+     * do so is clicked.
+     * @throws IOException should there be an issue with directories or input stream.
+     */
     @FXML
     protected void showMainViewScreen(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
