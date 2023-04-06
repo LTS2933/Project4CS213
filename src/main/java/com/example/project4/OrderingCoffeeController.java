b75ea206d936e8cb2fbe65736a59b51b9710e5f6
LTS2933 <116848955+LTS2933@users.noreply.github.com>
2023-04-06 10:36:22 -0400

    Update OrderingCoffeeController.java

--- src/main/java/com/example/project4/OrderingCoffeeController.java
+++ src/main/java/com/example/project4/OrderingCoffeeController.java
@@ -181,7 +181,11 @@ public class OrderingCoffeeController implements Initializable {
         coffeeError.setText("Successfully added coffee!");
         clear();
     }
-
+    /**
+     * Event handler that changes the view to the MainView when the button to
+     * do so is clicked.
+     * @throws IOException should there be an issue with directories or input stream.
+     */
     @FXML
     protected void showMainViewScreen(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
